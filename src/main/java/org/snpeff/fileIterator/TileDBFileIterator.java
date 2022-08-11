b9af6b6d2af8db745420ec6853131dfd20b2cb64
Jeremy Leipzig <leipzig@gmail.com>
2022-08-10 22:56:29 -0600

    stub out iterator

---
+++ src/main/java/org/snpeff/fileIterator/TileDBFileIterator.java
@@ -0,0 +1,27 @@
+package org.snpeff.fileIterator;
+
+import static io.tiledb.java.api.Datatype.*;
+import static io.tiledb.java.api.Layout.TILEDB_ROW_MAJOR;
+import static io.tiledb.java.api.Layout.TILEDB_UNORDERED;
+import static io.tiledb.java.api.QueryType.TILEDB_READ;
+import static io.tiledb.java.api.QueryType.TILEDB_WRITE;
+
+import io.tiledb.java.api.TileDBError;
+import java.io.File;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import org.apache.commons.io.FileUtils;
+
+
+public class TileDBFileIterator {
+    public void readTileDBfile() throws TileDBError, IOException {
+
+        // move old array to new temp folder for the upgrade.
+        String source = "examples/ash";
+        File srcDir = new File(source);
+    }
+}
