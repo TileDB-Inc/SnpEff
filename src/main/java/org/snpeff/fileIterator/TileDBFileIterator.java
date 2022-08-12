18ea794a7cc5805acc669b03210a4ca15e8aa3ad
Jeremy Leipzig <leipzig@gmail.com>
2022-08-11 22:49:49 -0600

    examples/tiledb

--- src/main/java/org/snpeff/fileIterator/TileDBFileIterator.java
+++ src/main/java/org/snpeff/fileIterator/TileDBFileIterator.java
@@ -16,6 +16,14 @@ import java.nio.file.Paths;
 import java.util.Map;
 import org.apache.commons.io.FileUtils;
 
+import static io.tiledb.java.api.Layout.TILEDB_GLOBAL_ORDER;
+import static io.tiledb.java.api.QueryType.TILEDB_READ;
+
+import io.tiledb.java.api.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class TileDBFileIterator {
     public void readTileDBfile() throws TileDBError, IOException {
@@ -23,5 +31,13 @@ public class TileDBFileIterator {
         // move old array to new temp folder for the upgrade.
         String source = "examples/ash";
         File srcDir = new File(source);
+
+        Context ctx = new Context();
+        Array my_sparse_array = new Array(ctx, "examples/tiledb_1sample/");
+        HashMap<String, Pair> dom = my_sparse_array.nonEmptyDomain();
+        for (Map.Entry<String, Pair> e : dom.entrySet()) {
+            System.out.println(
+                    e.getKey() + ": (" + e.getValue().getFirst() + ", " + e.getValue().getSecond() + ")");
+        }
     }
 }
