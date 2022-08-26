44821a4b5bc2d5af0dd7ac1d415e44dfc74fdd16
Jeremy Leipzig <leipzig@gmail.com>
2022-08-25 18:16:07 -0600

    JNI problems with tiledbvcf java

--- src/main/java/org/snpeff/fileIterator/TileDBFileIterator.java
+++ src/main/java/org/snpeff/fileIterator/TileDBFileIterator.java
@@ -10,6 +10,7 @@ import io.tiledb.java.api.TileDBError;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -20,6 +21,8 @@ import static io.tiledb.java.api.Layout.TILEDB_GLOBAL_ORDER;
 import static io.tiledb.java.api.QueryType.TILEDB_READ;
 
 import io.tiledb.java.api.*;
+
+import io.tiledb.libvcfnative.VCFReader;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -30,21 +33,36 @@ public class TileDBFileIterator {
 
         Context ctx = new Context();
         //Path arraysPath = Paths.get("src","test","resources","data","1.6","quickstart_sparse_array");
-        Path arraysPath = Paths.get("src","test","resources","data","ingested_2samples_GT_DP_PL","data");
+        //Path arraysPath = Paths.get("src","test","resources","data","ingested_2samples","data");
+        Path arraysPath = Paths.get("examples","hg002_tiledb","data");
         String arrayString =  "file://".concat(arraysPath.toAbsolutePath().toString());
 
         System.out.println(arrayString);
         Array my_sparse_array = new Array(ctx, arrayString, TILEDB_READ);
-        System.out.println("oh yeah");
 
         ArraySchema schema = my_sparse_array.getSchema();
 
+
+//        filter_ids: Attr<filter_ids,TILEDB_INT32,VAR>
+//        end_pos: Attr<end_pos,TILEDB_UINT32,1>
+//        real_start_pos: Attr<real_start_pos,TILEDB_UINT32,1>
+//        qual: Attr<qual,TILEDB_FLOAT32,1>
+//        fmt_DP: Attr<fmt_DP,TILEDB_UINT8,VAR>
+//        fmt_PL: Attr<fmt_PL,TILEDB_UINT8,VAR>
+//        id: Attr<id,TILEDB_CHAR,VAR>
+//        fmt_GT: Attr<fmt_GT,TILEDB_UINT8,VAR>
+//        fmt: Attr<fmt,TILEDB_UINT8,VAR>
+//        alleles: Attr<alleles,TILEDB_CHAR,VAR>
+//        info: Attr<info,TILEDB_UINT8,VAR>
         for (String name: schema.getAttributes().keySet()) {
             String key = name.toString();
             String value = schema.getAttributes().get(name).toString();
             System.out.println(key + ": " + value);
         }
 
+        Path VCFuri = Paths.get("examples","hg002_tiledb");
+        String VCFUriString =  "file://".concat(arraysPath.toAbsolutePath().toString());
+        VCFReader myVCFReader = new VCFReader(VCFUriString, null, null, null); //, String[] samples);, Optional< URI > samplesURI, Optional<String> config) {
 
         //query.setLayout(TILEDB_ROW_MAJOR);
     }
